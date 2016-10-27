2c454db63a8cc640b2f788c189063ac39011614f
kq5m9 <lazer321+github2@gmail.com>
2016-10-27 00:41:01 -0400

    module 2 - in progress ...

--- module2/script/app.js
+++ module2/script/app.js
@@ -1,6 +1,32 @@
 /*jslint node: true, plusplus: true*/
 /* For explanation of leading semicolon: https://github.com/airbnb/javascript/issues/21 */;
 
+//// SEE ASSIGNMENT NOTES - STEP 9 \\\\
+
 (function () {
   'use strict';
+  angular.module('ShoppingListCheckOff', [])
+         .controller('ToBuyController', ToBuyController)
+         .controller('AlreadyBoughtController', AlreadyBoughtController);
+  
+  ToBuyController.$inject = ['$scope', '$filter'];
+  function ToBuyController($scope, $filter) {
+    $scope.count = 0;
+    $scope.change = function () {
+      /* clears results if input changes */
+      $scope.result = "";
+      $scope.colorClass = "";
+    };
+  }
+    
+  AlreadyBoughtController.$inject = ['$scope', '$filter'];
+  function AlreadyBoughtController($scope, $filter) {
+    $scope.count = 0;
+    $scope.change = function () {
+      /* clears results if input changes */
+      $scope.result = "";
+      $scope.colorClass = "";
+    };
+  }
+  
 }());
